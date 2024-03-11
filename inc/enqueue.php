580264188905a507d86b86f51482cc220f1923e7
Celine Jacobs <celine@redrockmedia.nl>
2024-03-11 17:14:59 +0100

    Kleuren en font inladen

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,7 +11,8 @@ ADMIN ENQUEUE FUNCTIONS
 function far_script_enqueue()
 {
     //fonts and icons
-    wp_enqueue_style('Play', 'https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet', false);
+    wp_enqueue_style('Barlow', 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Rye&display=swap" rel="stylesheet', false);
+    wp_enqueue_style('Rye', 'https://fonts.googleapis.com/css2?family=Rye&display=swap" rel="stylesheet', false);
     wp_enqueue_style('fontawesome', 'https://kit.fontawesome.com/de3d3e56bd.css', false);
     //css
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/far.css', array(), '1.0', 'all');
